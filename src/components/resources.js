315b4d1869a22d25685195d6f1881db7da717de3
Andrew Dickinson <andrew.dickinson@t2.local>
2017-05-31 10:12:42 -0700

    Working basic Auth

---
+++ src/components/resources.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+export default class Resources extends Component{
+	render(){
+		return(
+			<div>
+				<h2> Super special secret Rub recipie</h2>
+				<ul>
+					<li> 1 Tbsp Brown Sugar</li>
+					<li> 1/2 Tbsp coriander</li>
+					<li> 1 Tbsp Cumin</li>
+					<li> 1 Tbsp Salt</li>
+					<li> 1/2 Tbsp Smoked paprika</li>
+					<li> 1 Tbsp Garlic Powder</li>
+					<li> 1/4 Tbsp Pepper</li>
+				</ul>
+			</div>
+			)
+	}
+}
